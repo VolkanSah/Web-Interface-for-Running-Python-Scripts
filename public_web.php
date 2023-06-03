786d4c5ec47e4ba4b351705de1d846da61619f57
Volkan Kücükbudak <VolkanSah@users.noreply.github.com>
2023-06-03 17:01:37 +0200

    Create public_web.php

---
+++ public_web.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Python Script Interface</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <label for="prompt">Prompt:</label>
+        <input type="text" id="prompt" name="prompt" required>
+        <br>
+        <button type="submit" name="submit">Submit</button>
+    </form>
+
+    <?php
+    if (isset($_POST['submit'])) {
+        // Python-Skript aufrufen und Ergebnis abrufen
+        $prompt = $_POST['prompt'];
+        $result = shell_exec("python3 your_script.py \"$prompt\"");
+
+        // Ergebnis anzeigen
+        echo "<h3>Result:</h3>";
+        echo "<pre>$result</pre>";
+    }
+    ?>
+</body>
+</html>
