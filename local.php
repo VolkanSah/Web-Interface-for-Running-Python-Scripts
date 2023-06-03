bbd1bc241a9730ae0b972eea32fe9f603e00dbbb
Volkan Kücükbudak <VolkanSah@users.noreply.github.com>
2023-06-03 17:02:05 +0200

    Create local.php

---
+++ local.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+  $("#submit").click(function(){
+    var prompt = $("#prompt").val();
+    $.post("your_script.py", {prompt: prompt}, function(data, status){
+      alert("Data: " + data + "\nStatus: " + status);
+    });
+  });
+});
+</script>
+</head>
+<body>
+
+<input type="text" id="prompt" placeholder="Enter your prompt">
+<button id="submit">Send</button>
+
+</body>
+</html>
